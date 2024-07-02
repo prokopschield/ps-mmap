6a46cc7115b0c20e89417210d4ca8eb099f5d14e
Prokop <prokop.schield@protonmail.com>
2024-07-02 12:01:13 +0200

    chore(src/rw.rs): impl TryFrom<MemoryMapping> for MutableMemoryMapping

--- src/rw.rs
+++ src/rw.rs
@@ -1,4 +1,6 @@
+use crate::error::Result;
 use crate::MemoryMapping;
+use crate::PsMmapError;
 use std::ops::Deref;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -22,6 +24,14 @@ impl<'lt> MutableMemoryMapping<'lt> {
 impl<'lt> From<MutableMemoryMapping<'lt>> for MemoryMapping<'lt> {
     fn from(mapping: MutableMemoryMapping<'lt>) -> Self {
         mapping.done()
+    }
+}
+
+impl<'lt> TryFrom<MemoryMapping<'lt>> for MutableMemoryMapping<'lt> {
+    type Error = PsMmapError;
+
+    fn try_from(mapping: MemoryMapping<'lt>) -> Result<Self> {
+        mapping.rw()
     }
 }
 
